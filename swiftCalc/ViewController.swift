855e78160b8446354a4a9f121c60b5023633e6dd
Lawrence Martin <lawrence.martin@cantire.com>
2017-01-23 21:57:38 -0500

    basic operations done

--- swiftCalc/ViewController.swift
+++ swiftCalc/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentNum = Double()
-    var operationResult = Double()
-    var displayNum:Int = 0
+    var operandStack = [Double]()
+    var operatorSym: String?
+    var userIsInTheMiddleOfTypingANumber = false
+    var digitHasDecimal = false
+    
+    // not used for user typing, just calculations
+    var displayValue: Double {
+        get {
+            return Double(displayNumber_lbl.text!)!
+        }
+        set {
+            // remove ".0" from display
+            if newValue == floor(newValue) {
+                displayNumber_lbl.text = "\(Int(newValue))"
+            } else {
+                displayNumber_lbl.text = "\(newValue)"
+            }
+            
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
     
     @IBOutlet weak var displayNumber_lbl: UILabel!
 
@@ -21,15 +39,56 @@ class ViewController: UIViewController {
         
         //displayNumber_lbl.text = ("\(displayNum)")
     }
-
-    @IBAction func numberInputHandler(_ sender: UIButton) {
-        
-        displayNumber_lbl.text = displayNumber_lbl.text! + sender.currentTitle!
-    }
     
 
+    @IBAction func operationInputHandler(_ sender: UIButton) {
+        userIsInTheMiddleOfTypingANumber = false
+        operatorSym = sender.currentTitle
+        operandStack.append(Double(displayNumber_lbl.text!)!)
+    }
+    
+    @IBAction func numberInputHandler(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            displayNumber_lbl.text! += digit
+        } else {
+            displayNumber_lbl.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func equalsInputHandler(_ sender: UIButton) {
+        
+        if operatorSym != nil {
+            userIsInTheMiddleOfTypingANumber = false
+            operandStack.append(displayValue)
+            
+            switch operatorSym! {
+            case "+": performOperation() { $0 + $1 }
+            case "-": performOperation() { $1 - $0 }
+            case "x": performOperation() { $0 * $1 }
+            case "/": performOperation() { $1 / $0 }
+                
+            default: break
+            }
+            
+            print("\(operandStack) " + operatorSym!)
+        }
+        
+    }
+    
     @IBAction func clearDisplay(_ sender: Any) {
-        displayNumber_lbl.text = ("\(displayNum)")
+        operandStack.removeAll()
+        userIsInTheMiddleOfTypingANumber = false
+        operatorSym = nil
+        displayValue = 0
+    }
+    
+    func performOperation( operation: (Double, Double) -> Double)  {
+        displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+        print("\(displayValue) ")
+        operandStack.append(displayValue)
     }
 
 }
